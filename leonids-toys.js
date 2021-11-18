bfb93edc885fda606995e860e5ea4ca10aabd62c
Seracrooce <fschmidt@live.com>
2021-11-18 15:27:57 -0600

    Completed the perry scope challenge

--- leonids-toys.js
+++ leonids-toys.js
@@ -1,87 +1,112 @@
 const toys = [
-    
-    {
-        id: 1,
-        name: "Paddle Ball Omega 4 - Extreme Edition",
-        maker: "Paddle Ball Inc. Ltd.",
-        color: "yellow",
-        price: 9.99,
-        weight: 2,
-        stock: 4,
-        operatingSystem: "PadBL 3"
-    },
+  {
+    id: 1,
+    name: "Paddle Ball Omega 4 - Extreme Edition",
+    maker: "Paddle Ball Inc. Ltd.",
+    color: "yellow",
+    price: 9.99,
+    weight: 2,
+    stock: 4,
+    operatingSystem: "PadBL 3",
+  },
 
-    {
-        id: 2,
-        name: "Hasbro Furby Connect Friend, Orange",
-        maker: "Hasbro",
-        model: "Connect Friend",
-        color: "orange",
-        weight: "Lovable",
-        price: 129.99,
-        stock: 1
-    },
+  {
+    id: 2,
+    name: "Hasbro Furby Connect Friend, Orange",
+    maker: "Hasbro",
+    model: "Connect Friend",
+    color: "orange",
+    weight: "Lovable",
+    price: 129.99,
+    stock: 1,
+  },
 
-    {
-        id: 3,
-        name: "Death Metal Susie",
-        maker: "Fluff n' Love Ltd.",
-        color: "black and red",
-        style: "Plush",
-        audioEnabled: true,
-        weight: 2,
-        stock: 10,
-        price: 19.99
-    }
-]
+  {
+    id: 3,
+    name: "Death Metal Susie",
+    maker: "Fluff n' Love Ltd.",
+    color: "black and red",
+    style: "Plush",
+    audioEnabled: true,
+    weight: 2,
+    stock: 10,
+    price: 19.99,
+  },
+];
 
 const ball = {
-    name: "Pink Round Ball",
-    maker: "We Make Balls",
-    color: "pink",
-    bouncy: true,
-    weight: 3.4,
-    stock: 14,
-    price: 5
-}
+  name: "Pink Round Ball",
+  maker: "We Make Balls",
+  color: "pink",
+  bouncy: true,
+  weight: 3.4,
+  stock: 14,
+  price: 5,
+};
 
 const frisbee = {
-    name: "Frisbee",
-    maker: "Frisbee Co.",
-    color: "frisbee blue",
-    weight: 0.5,
-    stock: 13,
-    price: 9.99
-}
+  name: "Frisbee",
+  maker: "Frisbee Co.",
+  color: "frisbee blue",
+  weight: 0.5,
+  stock: 13,
+  price: 9.99,
+};
 
 const addToyToInventory = (inventoryArray, toyObject) => {
-    //get index of highest id'd array object
-    const lastIndex = inventoryArray.length - 1;
-    // get the object
-    const currentLastToy = inventoryArray[lastIndex];
-    //get its id
-    const currMaxId = currentLastToy.id;
-    //put it on the new toy but plus 1
-    toyObject.id = currMaxId + 1;
-    inventoryArray.push(toyObject);
-}
+  //get index of highest id'd array object
+  const lastIndex = inventoryArray.length - 1;
+  // get the object
+  const currentLastToy = inventoryArray[lastIndex];
+  //get its id
+  const currMaxId = currentLastToy.id;
+  //put it on the new toy but plus 1
+  toyObject.id = currMaxId + 1;
+  inventoryArray.push(toyObject);
+};
 
 addToyToInventory(toys, ball);
 addToyToInventory(toys, frisbee);
 
-
 for (toy of toys) {
-    toy.price += toy.price * 0.05;
-    console.log(`The ${toy.color} ${toy.name} is $${toy.price}.`);
+  toy.price += toy.price * 0.05;
+  console.log(`The ${toy.color} ${toy.name} is $${toy.price}.`);
 }
 
 const removeProduct = (inventoryArray, idToRemove) => {
-    for (const item of inventoryArray) {
-        if (item.id === idToRemove) {
-            inventoryArray.splice(inventoryArray.indexOf(item), 1);
-        }
+  for (const item of inventoryArray) {
+    if (item.id === idToRemove) {
+      inventoryArray.splice(inventoryArray.indexOf(item), 1);
     }
-}
+  }
+};
 
 removeProduct(toys, 2);
 console.log(toys);
+
+//Perry Scope Work
+const currentInventory = [
+  "Muscle Man",
+  "Dora the Scientist",
+  "Princess Sparkles",
+  "Perry Scope",
+  "Explorer Backpack",
+  "Space Scout Suit",
+  "Geology Mineral Dig Kit",
+  "Perry Scope",
+];
+
+const sellPerryScope = () => {
+    let availableForPurchase;
+  for (const toy of currentInventory) {
+    if (toy === "Perry Scope") {
+      availableForPurchase = true;
+    }
+  }
+
+  if (availableForPurchase) {
+    console.log("Enjoy your new Perry Scope");
+  }
+};
+
+sellPerryScope();
